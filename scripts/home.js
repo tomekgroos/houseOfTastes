5f7d74c57502e343cb3b49b269217e65aab806e8
Your Name <tomek.groos@gmail.com>
2020-10-19 23:10:33 +0200

    poprawki mobilne

--- scripts/home.js
+++ scripts/home.js
@@ -1,33 +1,29 @@
-const container = document.querySelector(".container");
-const about = document.querySelector(".about");
-const recipes = document.querySelector(".recipes");
-const gallery = document.querySelector(".gallery");
-const title = document.querySelector(".title-text");
+ const container = document.querySelector(".container");
+    const about = document.querySelector(".about");
+    const recipes = document.querySelector(".recipes");
+    const gallery = document.querySelector(".gallery");
+    const title = document.querySelector(".title-text");
 
-window.onload = () => {
-  setTimeout(() => {
-    about.classList.remove("about");
-    recipes.classList.remove("recipes");
-    gallery.classList.remove("gallery");
-    title.classList.remove("title-text");
-    container.classList.remove("container");
+    setTimeout(() => {
+      about.classList.remove("about");
+      recipes.classList.remove("recipes");
+      gallery.classList.remove("gallery");
+      title.classList.remove("title-text");
+      container.classList.remove("container");
 
-    about.classList.add("aboutShow");
-    recipes.classList.add("recipesShow");
-    gallery.classList.add("galleryShow");
-    title.classList.add("title-textShow");
-    container.classList.add("containerShow");
-    
-  }, 2000);
+      about.classList.add("aboutShow");
+      recipes.classList.add("recipesShow");
+      gallery.classList.add("galleryShow");
+      title.classList.add("title-textShow");
+      container.classList.add("containerShow");
+    }, 1800);
 
     // adjusting elements on smaller screens in landscape mode
 
-    if (window.innerWidth < 1080 && window.matchMedia("(orientation:landscape)").matches){
+    if (
+      window.innerWidth < 1080 &&
+      window.matchMedia("(orientation:landscape)").matches
+    ) {
       document.getElementById("title-text-adjust").style.top = "-35vh";
       document.getElementById("recipes-adjust").style.marginTop = "15vh";
     }
-  
-
-    console.log(window.innerWidth);
-
-};
