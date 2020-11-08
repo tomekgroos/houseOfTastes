254fdb3da0fa4c8d3778a319160f2fda2394d2a2
Your Name <tomek.groos@gmail.com>
2020-11-09 00:50:41 +0100

    add image function working with adding classes

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,13 +1,27 @@
-
-const galleryArray = ['../img/gallery/food (1).jpeg', '../img/gallery/food (2).jpeg', 
-'../img/gallery/food (3).jpeg', '../img/gallery/food (4).jpeg'];
+const galleryArray = [
+  "../img/gallery/food (1).jpeg",
+  "../img/gallery/food (2).jpeg",
+  "../img/gallery/food (3).jpeg",
+  "../img/gallery/food (4).jpeg",
+];
 
 let imageBox = document.querySelector(".gallery-image-box");
 
-let addImageJs = () => {
-  let addImage = document.createElement('img');
-  imageBox.appendChild(addImage);
-  addImage.src =`${galleryArray[0]}`;
-}
+let addImage = () => {
+  // creating gallery elements
+  galleryArray.forEach((item) => {
+    let spanItem = document.createElement("span");
+    let addImage = document.createElement("img");
+    let catchBox = imageBox.appendChild(spanItem);
+    catchBox.appendChild(addImage);
+    // each image source comes from galleryArray
+    addImage.src = `${item}`;
+  });
+  // catching all spans and add classes
+  const spans = document.querySelectorAll("span");
+  const spansArr = [...spans];
 
-
+  for (let i = 0; i < spansArr.length; i++) {
+    spansArr[i].classList.add(`--image:${i + 1}`);
+  }
+};
