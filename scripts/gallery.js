f1c912a63b5cb2bfc8589b2a997dd9db20f49c8a
Your Name <tomek.groos@gmail.com>
2020-11-21 22:55:46 +0100

    responsive image gallery almost done

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -17,12 +17,33 @@ const galleryArray = [
   "../img/gallery/food (16).jpg",
   "../img/gallery/food (17).jpg",
   "../img/gallery/food (18).jpg",
+  "../img/gallery/food (19).jpg",
+  "../img/gallery/food (20).jpg",
+  "../img/gallery/food (21).jpg",
+  "../img/gallery/food (22).jpg",
+  "../img/gallery/food (23).jpg",
+  "../img/gallery/food (24).jpg",
+  "../img/gallery/food (25).jpg",
+  "../img/gallery/food (26).jpg",
+  "../img/gallery/food (27).jpg",
+  "../img/gallery/food (28).jpg",
+  "../img/gallery/food (29).jpg",
+  "../img/gallery/food (30).jpg",
+  "../img/gallery/food (31).jpg",
+  "../img/gallery/food (32).jpg",
+  "../img/gallery/food (33).jpg",
+  "../img/gallery/food (34).jpg",
+  "../img/gallery/food (35).jpg",
+  "../img/gallery/food (36).jpg",
+  "../img/gallery/food (37).jpg",
+  "../img/gallery/food (38).jpg",
+  "../img/gallery/food (39).jpg",
+  "../img/gallery/food (40).jpg",
 ];
 
-let container = document.querySelector(".container");
+const container = document.querySelector(".container");
 
 let addImage = () => {
-  
   galleryArray.forEach((item) => {
     // creating gallery elements
     let imageContainer = document.createElement("div");
@@ -44,20 +65,27 @@ let addImage = () => {
     addText.innerHTML = "click to zoom";
     // each image source comes from galleryArray
     addImage.src = `${item}`;
+
   });
   // catching all divs with image-box class and all img elements
-  const imageBoxes = document.querySelectorAll(".image-box");
+  const imageContainers = document.querySelectorAll(".gallery-image-container");
   const images = document.querySelectorAll("img");
   // create an array from them
-  const imageBoxesArr = [...imageBoxes];
+  const imageContainersArr = [...imageContainers];
   const imagesArr = [...images];
   // add id attribute for each image box and alt for each image
-  for (let i = 0; i < imageBoxesArr.length; i++) {
-   imageBoxesArr[i].setAttribute("id", `image${i+1}`);
-   imagesArr[i].setAttribute("alt", `food${i+1}`);
+  for (let i = 0; i < imageContainersArr.length; i++) {
+    imageContainersArr[i].setAttribute("id", `image${i + 1}`);
+    imagesArr[i].setAttribute("alt", `food${i + 1}`);
+    // add a random size of each gallery-image-container
+    let randomSpan = Math.ceil(Math.random() * 2);
+    imageContainersArr[i].classList.add(`width-col-${randomSpan}`);
+    imageContainersArr[i].classList.add(`height-row-${randomSpan}`);
   }
-
+ 
 };
+
+
 
 /* zadanie na dzis, jest po polnocy :D 
 nadaj losowy grid-column span dla kazdego ze zdjec,
